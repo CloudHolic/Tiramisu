0354d713016326c6e26e8079e7277c7d69f8f3ce
CloudHolic <bluegkstjr@kaist.ac.kr>
2018-03-12 15:28:49 +0900

    Add an alarm command.

--- Tiramisu/Bot.cs
+++ Tiramisu/Bot.cs
@@ -106,6 +106,7 @@ namespace Tiramisu
             _cnext.RegisterCommands<Commands.RateChange>();
             _cnext.RegisterCommands<Commands.UserInfo>();
             _cnext.RegisterCommands<Commands.Dice>();
+            _cnext.RegisterCommands<Commands.Alarm>();
 
             // Hook some events for logging.
             _client.Ready += OnReadyAsync;
---
+++ Tiramisu/Commands/Alarm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Timers;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using NLog;
+
+namespace Tiramisu.Commands
+{
+    internal class Alarm
+    {
+        private static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
+        [Command("alarm"), Aliases("a")]
+        [Description("Set an alarm.")]
+        public async Task SetAlarm(CommandContext ctx, [Description("Minutes")]int min, [Description("Subject")]string subject, [Description("Participants")]params string[] participants)
+        {
+            Log.Info($"Check alarm command - {ctx.Message.Content}");
+
+            await ctx.TriggerTypingAsync();
+
+            var timer = new Timer
+            {
+                Interval = min * 60 * 1000,
+                AutoReset = false
+            };
+            timer.Elapsed += (sender, e) => TimerElapsed(ctx, subject, participants.ToList());
+            timer.Start();
+
+            await ctx.RespondAsync($"{ctx.Member.DisplayName} set a new alarm.");
+        }
+
+        private async void TimerElapsed(CommandContext ctx, string subject, List<string> participantList)
+        {
+            Log.Info($"Alarm executed - {ctx.Message.Content}");
+
+            var idList = (from member in ctx.Guild.Members where participantList.Contains(member.DisplayName) select member.Id).ToList();
+            var mention = $"{ctx.Member.DisplayName} set an alarm at {ctx.Message.Timestamp:t} to";
+            foreach (var id in idList)
+                mention += $" <@{id}>,";
+            mention = mention.TrimEnd(',');
+            await ctx.RespondAsync(mention + $"\n {subject}");
+        }
+    }
+}
